3ed47cc83d27a01ecb166e3879c4e529df89cf2e
Anamika596-raj <anamikaraj022@gmail.com>
2026-09-09 07:36:37 +0530

    Prepare project for deployment

--- event_registration/settings.py
+++ event_registration/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-skkz)b5&(v(5!pily3%lf6%c6_h9v=1v=vavei9d1+&anlpk&h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -114,7 +114,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Email
